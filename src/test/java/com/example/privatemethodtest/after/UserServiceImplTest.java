853a36e4d5a3aa9ec77bb9f80e4b8bd89d88c030
Koji NISHIYAMA <k_nishiyama@fullspeed.co.jp>
2022-12-21 22:08:34 +0900

    Remove unused annotation

--- src/test/java/com/example/privatemethodtest/after/UserServiceImplTest.java
+++ src/test/java/com/example/privatemethodtest/after/UserServiceImplTest.java
@@ -12,11 +12,8 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class UserServiceImplTest {
 
